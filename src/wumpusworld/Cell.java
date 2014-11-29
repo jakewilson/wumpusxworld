467f42f6829d2b5a0c0a9a8e87082a24d6b8f16c
Jake Wilson <jakewilsonfl@gmail.com>
2014-11-28 22:45:11 -0500

    Adds Cell class.

--- src/wumpusworld/Cell.java
+++ src/wumpusworld/Cell.java
@@ -6,6 +6,39 @@ package wumpusworld;
  * @version Nov 28, 2014
  */
 public class Cell {
+	
+  /** Possible states of a cell */
+  public static final int EMPTY   = 0,
+                          BREEZE  = 1,
+                          PIT     = 2,
+                          STENCH  = 4,
+                          WUMPUS  = 8,
+                          GLITTER = 16;
+  
+  private int content; // what is contained in the cell
+  
+  /**
+   * Constructs and initializes a new Cell with EMPTY content
+   */
+  public Cell() {
+    content = EMPTY;
+  }
+  
+  /**
+   * Adds content to the cell
+   * @param c the content to add
+   */
+  public void addContent(int c) {
+    content += c;
+  }
+  
+  /**
+   * Returns the content of the cell
+   * @return the content of the cell
+   */
+  public int getContent() {
+    return content;
+  }
 
 }
 
